5c0e4e610e7663acfa7423c9aabbf12287deeb8e
Cássio <cassio375@gmail.com>
2022-06-02 21:40:14 -0300

    Teste em Variavel de Ambiente

---
+++ README.md
@@ -0,0 +1,12 @@
+## Métodos REST
+
+No projeto sera utilizado os métodos GET, POST, PUT e DELETE
+para fins de apredizado
+
+
+## Empacotamento
+
+Através deste projeto executei as funções de
+empacotamento que possibilitam o deploy da 
+aplicação e a utilização em outras máquinas com 
+mais facilidade, podendo gerar arquivos .jar e .war
--- src/main/java/com/cassio/learnspring/controller/HelloController.java
+++ src/main/java/com/cassio/learnspring/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.cassio.learnspring.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${ENV_DB_URL:Blank}")
+    private String dbEnviromentVariable;
+
     @GetMapping("/oi")
     public String helloMessage(){
         return "Hello, i am Cassio";
     }
+
+    @GetMapping("/ambient")
+    public String getDbEnviromentVariable(){
+        return "The ambient variable is: " +dbEnviromentVariable;
+    }
+
+
+
 }
